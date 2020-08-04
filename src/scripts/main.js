8dc958cf5e6cc6f716bb9be0db80a39c0f4a90f1
Candido Souza <candidosouzza@gmail.com>
2020-08-04 08:57:21 -0300

    Importando o componente collapse do bootstrap

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,11 @@ try {
     window.$ = window.jQuery = require('jquery');
     // require('bootstrap');
     require('bootstrap/js/dist/dropdown');
+    require('bootstrap/js/dist/collapse');
 
-    $(document).ready(function () {});
+    $(document).ready(function () {
+        $('.collapse').collapse('show');
+    });
 } catch (e) {
     console.log(e);
 }
